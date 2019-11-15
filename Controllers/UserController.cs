78a3c056922f7cf53f04eccd31370bac7e03fcb8
ramezrb <ramez.rb@gmail.com>
2019-11-15 23:57:38 +0200

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AWP_HW.Controllers
+{
+	public class UserController : Controller
+	{
+		public ActionResult Login()
+		{
+			return View();
+		}
+
+		public ActionResult Registration()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+	}
+}
